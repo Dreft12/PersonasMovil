b896ca0dd5363d595707232ee80949bd5c3cd0c8
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-06 21:01:59 -0500

    V 1.0 Aplicacion funcional
    Aplicacion funcionando correctamente. modificando funcionalidades principales tales como registar, listar y los strings

--- app/src/main/java/com/manillas/personasmovil/Listar.java
+++ app/src/main/java/com/manillas/personasmovil/Listar.java
@@ -2,12 +2,42 @@ package com.manillas.personasmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class Listar extends AppCompatActivity {
-
+    private TableLayout tabla;
+    private ArrayList<Persona> personas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
+        tabla = findViewById(R.id.tabla);
+        personas = Datos.obtener();
+
+        for (int i = 0; i < personas.size(); i++){
+            TableRow row = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+            TextView c4 = new TextView(this);
+            c1.setText(String.valueOf(i+1));
+            c2.setText(personas.get(i).getCedula());
+            c3.setText(personas.get(i).getNombre());
+            c4.setText(personas.get(i).getApellido());
+
+            row.addView(c1);
+            row.addView(c2);
+            row.addView(c3);
+            row.addView(c4);
+
+            tabla.addView(row);
+        }
     }
 }
--- app/src/main/java/com/manillas/personasmovil/Registrar.java
+++ app/src/main/java/com/manillas/personasmovil/Registrar.java
@@ -2,12 +2,39 @@ package com.manillas.personasmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registrar extends AppCompatActivity {
-
+    private EditText cedula, nombre, apellido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        cedula = findViewById(R.id.txtcedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+    }
+
+    public void guardar(View v){
+        String cedula, nombre, apellido;
+        cedula = this.cedula.getText().toString();
+        nombre = this.nombre.getText().toString();
+        apellido = this.apellido.getText().toString();
+        Persona p = new Persona(cedula, nombre, apellido);
+        p.guardar();
+        Toast.makeText(this, getResources().getText(R.string.exitoso), Toast.LENGTH_SHORT).show();
+        limpiar();
+    }
+
+    public void borrar(View v){
+        limpiar();
+    }
+    private void limpiar(){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
     }
 }

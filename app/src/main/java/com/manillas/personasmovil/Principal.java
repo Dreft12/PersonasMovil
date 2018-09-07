2b1c6ed6daa4e4775e9d5afc1bc08c4ff428d8a4
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-06 20:22:26 -0500

    V 0.5 Agregando clases principales
    Se agregaron las clases de Persona, Datos y nuevos activitys

--- app/src/main/java/com/manillas/personasmovil/Principal.java
+++ app/src/main/java/com/manillas/personasmovil/Principal.java
@@ -1,13 +1,26 @@
 package com.manillas.personasmovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
-
+    private ListView lv;
+    private String [] opc;
+    private Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
     }
 }
